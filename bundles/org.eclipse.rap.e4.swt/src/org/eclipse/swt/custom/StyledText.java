f68178c9fbfcdd313caafec4565a72b133266e2a
Tom Schindl <tom.schindl@bestsolution.at>
2014-02-21 12:03:12 +0100

    add not yet supported widgets

---
+++ bundles/org.eclipse.rap.e4.swt/src/org/eclipse/swt/custom/StyledText.java
@@ -0,0 +1,23 @@
+package org.eclipse.swt.custom;
+
+import org.eclipse.swt.widgets.Canvas;
+import org.eclipse.swt.widgets.Composite;
+
+public class StyledText extends Canvas {
+
+	public StyledText(Composite parent, int style) {
+		super(parent, style);
+	}
+
+	public void removeVerifyKeyListener(
+			VerifyKeyListener keyListener) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void addVerifyKeyListener(
+			VerifyKeyListener keyListener) {
+		// TODO Auto-generated method stub
+		
+	}
+}
